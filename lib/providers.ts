833613b1e4739f6ad63b31952b311a5189ff086e
Darren Cooney <darren.cooney@gmail.com>
2023-01-03 16:17:58 -0500

    Adding fix for broken author links

--- lib/providers.ts
+++ lib/providers.ts
@@ -3,7 +3,7 @@ export default {
     api_var: 'client_id',
     arr_key: 'results',
     search_var: 'query',
-    base_url: 'https:unsplash.com',
+    base_url: 'https://unsplash.com',
     api: {
       photos: 'https://api.unsplash.com/photos/',
       search: 'https://api.unsplash.com/search/photos/'
@@ -17,7 +17,7 @@ export default {
     api_var: 'key',
     arr_key: 'hits',
     search_var: 'q',
-    base_url: 'https:pixabay.com',
+    base_url: 'https://pixabay.com',
     api: {
       photos: 'https://pixabay.com/api/',
       search: 'https://pixabay.com/api/'
@@ -29,7 +29,7 @@ export default {
   pexels: {
     api_var: 'key',
     arr_key: 'photos',
-    base_url: 'https:pexels.com',
+    base_url: 'https://pexels.com',
     search_var: 'query',
     api: {
       photos: 'https://api.pexels.com/v1/curated/',
